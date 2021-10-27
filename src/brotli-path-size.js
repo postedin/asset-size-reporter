010df981b641af7fa241d30549678e35d01f387d
Robert Clancy (Robbo) <robclancy@users.noreply.github.com>
2021-10-27 01:53:33 -0300

    Update brotli-path-size.js

--- src/brotli-path-size.js
+++ src/brotli-path-size.js
@@ -4,17 +4,10 @@ const execa = require('execa');
 const Counter = require('passthrough-counter');
 
 module.exports = async (path, { level } = {}) => new Promise((resolve, reject) => {
-  let counter = new Counter();
-
-  let promise = execa('brotli', ['-c', `-${level !== undefined ? level : 'Z'}`, '-k', path]);
-
-  let stream = promise.stdout;
-  stream.on('error', reject);
-  stream.pipe(counter).once('finish', () => {
-    stream.end();
-
-    promise
-      .then(() => resolve(counter.length))
-      .catch(reject);
+  const { stdout } = execa('brotli', ['-c', `-${level !== undefined ? level : 'Z'}`, path]);
+  
+  stdout.on('error', reject);
+  stdout.pipe(Counter).on('finish', function() {
+    resolve(this.length);
   });
 });
